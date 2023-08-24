15d565f43e7daf042c58687a5d032dc3782bb13f
pyomelch <omelch.py@gmail.com>
2023-08-24 16:51:56 +0300

    Added @api_views with "GET" & "POST" to crew_list

--- flights/views.py
+++ flights/views.py
@@ -1,12 +1,23 @@
-from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 from flights.models import Crew
 from flights.serializers import CrewSerializer
 
 
+@api_view(["GET", "POST"])
 def crew_list(request):
     if request.method == "GET":
         crews = Crew.objects.all()
         serializer = CrewSerializer(crews, many=True)
-        return JsonResponse(serializer.data, status=200, safe=False)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
+    if request.method == "POST":
+        serializer = CrewSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
