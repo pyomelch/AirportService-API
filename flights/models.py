25cfe41328003c11112c33d7a7f94ea93c715082
pyomelch <omelch.py@gmail.com>
2023-08-24 13:22:06 +0300

    Added model Crew to flights

--- flights/models.py
+++ flights/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Crew(models.Model):
+    first_name = models.CharField(max_length=60)
+    last_name = models.CharField(max_length=60)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
