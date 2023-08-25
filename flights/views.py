b60245259a0ed91921a96119b0ba09b3d6983dff
pyomelch <omelch.py@gmail.com>
2023-08-25 13:21:30 +0300

    Added @api_views with "GET" & "PUT" & "DELETE" to crew_detail

--- flights/views.py
+++ flights/views.py
@@ -21,3 +21,26 @@ def crew_list(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["GET", "PUT", "DELETE"])
+def crew_detail(request, pk):
+    try:
+        crew = Crew.objects.get(pk=pk)
+    except Crew.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == "GET":
+        serializer = CrewSerializer(crew)
+        return Response(serializer.data)
+
+    elif request.method == "PUT":
+        serializer = CrewSerializer(crew, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == "DELETE":
+        Crew.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
