ce35311d827f5b573a4685884855acf0a536f58e
pyomelch <omelch.py@gmail.com>
2023-08-25 16:18:42 +0300

    Reformat CrewList & CrewDetail GenericViews

--- flights/views.py
+++ flights/views.py
@@ -1,39 +1,14 @@
-from rest_framework import generics, mixins
+from rest_framework import generics
 
 from flights.models import Crew
 from flights.serializers import CrewSerializer
 
 
-class CrewList(
-    mixins.ListModelMixin,
-    mixins.CreateModelMixin,
-    generics.GenericAPIView
-):
+class CrewList(generics.ListCreateAPIView):
     queryset = Crew.objects.all()
     serializer_class = CrewSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class CrewDetail(
-    mixins.RetrieveModelMixin,
-    mixins.UpdateModelMixin,
-    mixins.DestroyModelMixin,
-    generics.GenericAPIView
-):
+class CrewDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Crew.objects.all()
     serializer_class = CrewSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
-
