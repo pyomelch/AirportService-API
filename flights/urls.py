a1d5b5b33b119bde66bf13f92980d0d02d1d854a
pyomelch <omelch.py@gmail.com>
2023-08-25 14:20:11 +0300

    Added CrewDetail with APIView

--- flights/urls.py
+++ flights/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from flights.views import crew_list
+from flights.views import CrewList, CrewDetail
 
 urlpatterns = [
-   path("crews/", crew_list, name="crew-list")
+   path("crews/", CrewList.as_view(), name="crew-list"),
+   path("crews/<int:pk>/", CrewDetail.as_view(), name="crew-detail"),
 ]
 
 app_name = "flights"
--- flights/views.py
+++ flights/views.py
@@ -1,46 +1,50 @@
-from rest_framework.decorators import api_view
+from django.http import Http404
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.views import APIView
 
 from flights.models import Crew
 from flights.serializers import CrewSerializer
 
 
-@api_view(["GET", "POST"])
-def crew_list(request):
-    if request.method == "GET":
+class CrewList(APIView):
+    def get(self, request):
         crews = Crew.objects.all()
         serializer = CrewSerializer(crews, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    if request.method == "POST":
+    def post(self, request):
         serializer = CrewSerializer(data=request.data)
 
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(["GET", "PUT", "DELETE"])
-def crew_detail(request, pk):
-    try:
-        crew = Crew.objects.get(pk=pk)
-    except Crew.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+class CrewDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Crew.objects.get(pk=pk)
+        except Crew.DoesNotExist:
+            raise Http404
 
-    if request.method == "GET":
+    def get(self, request, pk):
+        crew = self.get_object(pk)
         serializer = CrewSerializer(crew)
         return Response(serializer.data)
 
-    elif request.method == "PUT":
+    def put(self, request, pk):
+        crew = self.get_object(pk)
         serializer = CrewSerializer(crew, data=request.data)
+
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == "DELETE":
-        Crew.delete()
+    def delete(self, request, pk):
+        crew = self.get_object(pk)
+        crew.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
