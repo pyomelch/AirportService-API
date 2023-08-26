32d3d725d0f390c2a4df66432f04cc314c48ff14
pyomelch <omelch.py@gmail.com>
2023-08-26 12:25:07 +0300

    Refactored views with ModelViewSet

--- flights/views.py
+++ flights/views.py
@@ -1,17 +1,10 @@
-from rest_framework import mixins, viewsets
+from rest_framework import viewsets
 
 from flights.models import Crew
 from flights.serializers import CrewSerializer
 
 
-class CrewViewSet(
-    mixins.ListModelMixin,
-    mixins.CreateModelMixin,
-    mixins.RetrieveModelMixin,
-    mixins.UpdateModelMixin,
-    mixins.DestroyModelMixin,
-    viewsets.GenericViewSet
-):
+class CrewViewSet(viewsets.ModelViewSet):
     queryset = Crew.objects.all()
     serializer_class = CrewSerializer
 
