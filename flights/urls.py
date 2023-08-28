417b627dcab8ac39c3618d9425fc8fd23c1f04b7
pyomelch <omelch.py@gmail.com>
2023-08-28 15:26:12 +0300

    Added viewsets and urls

--- flights/urls.py
+++ flights/urls.py
@@ -2,10 +2,25 @@ from django.urls import path, include
 
 from rest_framework import routers
 
-from flights.views import CrewViewSet
+from flights.views import (
+   CrewViewSet,
+   AirplaneTypeViewSet,
+   AirplaneViewSet,
+   AirportViewSet,
+   RouteViewSet,
+   FlightViewSet,
+   OrderViewSet,
+)
+
 
 router = routers.DefaultRouter()
 router.register("crews", CrewViewSet)
+router.register("airplane-types", AirplaneTypeViewSet)
+router.register("airplanes", AirplaneViewSet)
+router.register("airports", AirportViewSet)
+router.register("routes", RouteViewSet)
+router.register("flights", FlightViewSet)
+router.register("orders", OrderViewSet)
 
 urlpatterns = [
    path("", include(router.urls))
--- flights/views.py
+++ flights/views.py
@@ -1,10 +1,63 @@
 from rest_framework import viewsets
 
-from flights.models import Crew
-from flights.serializers import CrewSerializer
+from flights.models import (
+    Crew,
+    AirplaneType,
+    Airplane,
+    Airport,
+    Route,
+    Flight,
+    Order,
+)
+from flights.serializers import (
+    CrewSerializer,
+    AirplaneTypeSerializer,
+    AirplaneSerializer,
+    AirportSerializer,
+    RouteSerializer,
+    FlightSerializer,
+    OrderSerializer,
+)
 
 
 class CrewViewSet(viewsets.ModelViewSet):
     queryset = Crew.objects.all()
     serializer_class = CrewSerializer
+
+
+class AirplaneTypeViewSet(viewsets.ModelViewSet):
+    queryset = AirplaneType.objects.all()
+    serializer_class = AirplaneTypeSerializer
+
+
+class AirplaneViewSet(viewsets.ModelViewSet):
+    queryset = Airplane.objects.select_related("type")
+    serializer_class = AirplaneSerializer
+
+
+class AirportViewSet(viewsets.ModelViewSet):
+    queryset = Airport.objects.all()
+    serializer_class = AirportSerializer
+
+
+class RouteViewSet(viewsets.ModelViewSet):
+    queryset = Route.objects.select_related(
+        "source",
+        "destination"
+    )
+    serializer_class = RouteSerializer
+
+
+class FlightViewSet(viewsets.ModelViewSet):
+    queryset = Flight.objects.select_related(
+        "airplane",
+        "route__source",
+        "route__destination"
+    )
+    serializer_class = FlightSerializer
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
 
