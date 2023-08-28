f2839ca79b0f441254357576dc5f97edbeac7e99
pyomelch <omelch.py@gmail.com>
2023-08-28 10:46:16 +0300

    Refactored serializer with ModelSerializer

--- flights/serializers.py
+++ flights/serializers.py
@@ -3,16 +3,7 @@ from rest_framework import serializers
 from flights.models import Crew
 
 
-class CrewSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    first_name = serializers.CharField(required=True)
-    last_name = serializers.CharField(required=True)
-
-    def create(self, validated_data):
-        return Crew.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.first_name = validated_data.get("first_name", instance.first_name)
-        instance.last_name = validated_data.get("last_name", instance.last_name)
-        instance.save()
-        return instance
+class CrewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Crew
+        fields = "__all__"
