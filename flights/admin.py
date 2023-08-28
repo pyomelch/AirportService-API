f4caf18543128665fe962e6adc1cc60b2ab5197a
pyomelch <omelch.py@gmail.com>
2023-08-28 12:30:57 +0300

    Registered new models in admin & added TicketInLine

--- flights/admin.py
+++ flights/admin.py
@@ -1,6 +1,32 @@
 from django.contrib import admin
 
-from flights.models import Crew
+from flights.models import (
+    Crew,
+    Airport,
+    Route,
+    AirplaneType,
+    Airplane,
+    Flight,
+    Order,
+    Ticket
+)
+
+
+class TicketInLine(admin.TabularInline):
+    model = Ticket
+    extra = 1
+
+
+@admin.register(Order)
+class Order(admin.ModelAdmin):
+    inlines = (TicketInLine,)
+
 
 admin.site.register(Crew)
+admin.site.register(Airport)
+admin.site.register(Route)
+admin.site.register(AirplaneType)
+admin.site.register(Airplane)
+admin.site.register(Flight)
+admin.site.register(Ticket)
 
